1502d056125a02ce6de79e68fdb8d5a252f960f1
soralit <soralitria@gmail.com>
2023-09-05 15:46:59 +0800

    refactor: anti_tamper

--- hardware_interface/anti_tamper.h
+++ hardware_interface/anti_tamper.h
@@ -13,13 +13,11 @@
 #include "stdbool.h"
 #include "err_code.h"
 
-
 void TamperStartup(void);
 void TamperIntHandler(void);
 void TamperBackgroundHandler(void);
 bool Tampered(void);
 
 void TamperTest(int argc, char *argv[]);
-
 
 #endif

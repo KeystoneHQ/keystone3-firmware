7eb27de3a8d8d4f8bc0c9ed99c138b697f879b6a
soralit <soralitria@gmail.com>
2023-09-05 15:22:26 +0800

    refactor: motor_manager

--- business/managers/motor_manager.c
+++ business/managers/motor_manager.c
@@ -2,13 +2,7 @@
 #include "motor_manager.h"
 #include "drv_motor.h"
 #include "device_setting.h"
-
-static void MicroVibration(void);
-static void SlightVibration(void);
-static void ShortVibration(void);
-static void LongVibration(void);
-static void SuperLongVibration(void);
-
+#include "hardware_interface.h"
 
 void Vibrate(VIBRATION_STRENGTH strength)
 {
@@ -60,26 +54,4 @@ void UnlimitedVibrate(VIBRATION_STRENGTH strength)
         printf("not support current strength %d\n", strength);
         break;
     }
-}
-
-static void MicroVibration(void)
-{
-    //MotorCtrl(30, 200);
-}
-static void SlightVibration(void)
-{
-    MotorCtrl(MOTOR_LEVEL_LOW, MOTOR_SHAKE_SHORT_TIME);
-}
-static void ShortVibration(void)
-{
-    MotorCtrl(MOTOR_LEVEL_MIDDLE, MOTOR_SHAKE_SHORT_TIME);
-}
-static void LongVibration(void)
-{
-    MotorCtrl(MOTOR_LEVEL_MIDDLE, MOTOR_SHAKE_LONG_TIME);
-
-}
-static void SuperLongVibration(void)
-{
-    MotorCtrl(MOTOR_LEVEL_MIDDLE, MOTOR_SHAKE_ULTRA_LONG_TIME);
 }

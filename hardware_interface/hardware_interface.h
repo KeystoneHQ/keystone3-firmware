5deb154032ad2b8d0c3727ccb3a8bf400ced28db
soralit <soralitria@gmail.com>
2023-09-05 15:22:26 +0800

    refactor: motor_manager

--- hardware_interface/hardware_interface.h
+++ hardware_interface/hardware_interface.h
@@ -1,4 +1,12 @@
 #ifndef _HARDWARE_INTERFACE_H_
 #define _HARDWARE_INTERFACE_H_
 
+// START: Motor
+void MicroVibration(void);
+void SlightVibration(void);
+void ShortVibration(void);
+void LongVibration(void);
+void SuperLongVibration(void);
+// END: Motor
+
 #endif

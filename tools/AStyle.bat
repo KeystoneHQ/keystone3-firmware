e63807119f8c11dd6c7046b470e949be6c95b409
stonewang <15809188520@163.com>
2023-09-11 17:49:08 +0800

    fix:Modify script

--- tools/AStyle.bat
+++ tools/AStyle.bat
@@ -1,3 +1,3 @@
-astyle -A3nrUpHcQ --exclude=../cm_backtrace/Languages --exclude=../components/sqlite3/sqlite3.h --exclude=../components/sqlite3/sqlite3.c --exclude=../external --exclude=../rust "../*.c" "../*.h" "../*.cpp"
+astyle -A3nrUpHcQ --exclude=../src/cm_backtrace/Languages ../src/*.c ../src/*.h
 
 pause

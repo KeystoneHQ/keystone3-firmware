2ac8a1fb20a40ec1e87a595ec246f225f1c7842a
soralit <soralitria@gmail.com>
2023-09-13 15:03:10 +0800

    refactor: extract se_interface and se_manager forom keystore

---
+++ src/hardware_interface/se_interface.h
@@ -0,0 +1,17 @@
+#ifndef _SE_INTERFACE_H_
+#define _SE_INTERFACE_H_
+
+#include "stdint.h"
+#include "stdlib.h"
+
+int32_t SE_EncryptWrite(uint8_t slot, uint8_t block, const uint8_t *data);
+int32_t SE_Kdf(uint8_t slot, const uint8_t *authKey, const uint8_t *inData, uint32_t inLen, uint8_t *outData);
+int32_t SE_DeriveKey(uint8_t slot, const uint8_t *authKey);
+int32_t SE_HmacEncryptRead(uint8_t *data, uint8_t page);
+int32_t SE_HmacEncryptWrite(const uint8_t *data, uint8_t page);
+int32_t SE_GetDS28S60Rng(uint8_t *rngArray, uint32_t num);
+int32_t SE_GetTRng(void *buf, uint32_t len);
+int32_t SE_GetAtecc608bRng(uint8_t *rngArray, uint32_t num);
+
+
+#endif

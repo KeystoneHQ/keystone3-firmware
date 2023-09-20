bd4495666bc86054f098b4d651bb2650ea368794
bmy <bianminyan@163.com>
2023-09-20 15:33:58 +0800

    update version

--- src/config/version.h
+++ src/config/version.h
@@ -9,8 +9,8 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-#define SOFTWARE_VERSION_MAJOR              0
-#define SOFTWARE_VERSION_MINOR              9
+#define SOFTWARE_VERSION_MAJOR              1
+#define SOFTWARE_VERSION_MINOR              0
 #define SOFTWARE_VERSION_BUILD              0
 #define SOFTWARE_VERSION                    (SOFTWARE_VERSION_MAJOR * 10000 + SOFTWARE_VERSION_MINOR * 100 + SOFTWARE_VERSION_BUILD)
 

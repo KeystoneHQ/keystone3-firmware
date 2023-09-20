2f8d900a86571a87167527c2b21374fbadfb564a
Charon-Fan <Charon.Dian@gmail.com>
2023-09-20 18:30:27 +0800

    feat: Add CMD_CHECK_LOCK_STATUS command

--- src/webusb_protocol/apdu_services/service_eth_sign.c
+++ src/webusb_protocol/apdu_services/service_eth_sign.c
@@ -1,4 +1,13 @@
 #include "service_eth_sign.h";
+#include "user_delay.h";
+#include "gui_chain.h";
+#include "gui_views.h";
+
+typedef struct
+{
+    uint8_t viewType;
+    uint8_t urType;
+} UrViewType_t;
 
 Response *ProcessEthereumTransactionSignature(uint8_t *data, uint16_t dataLen)
 {
@@ -11,6 +20,14 @@ Response *ProcessEthereumTransactionSignature(uint8_t *data, uint16_t dataLen)
     int len = GetMnemonicType() == MNEMONIC_TYPE_BIP39 ? sizeof(seed) : GetCurrentAccountEntropyLen();
     GetAccountSeed(GetCurrentAccountIndex(), seed, SecretCacheGetPassword());
     struct URParseResult *urResult = parse_ur(data);
+    UrViewType_t urViewType = {0, 0};
+    urViewType.viewType = urResult->t;
+    urViewType.urType = urResult->ur_type;
+
+    GuiFrameOpenView(&g_qrCodeView);
+    handleURResult(urResult, urViewType, false);
+
+    UserDelay(5000);
     // TODO: check error / multiple ur / define error code
     encodeResult = eth_sign_tx(urResult->data, seed, len);
     response->data = encodeResult->data;

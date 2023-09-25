a4a20ebf94bd96157373527859095da5d1d3a7de
soralit <soralitria@gmail.com>
2023-09-25 14:23:52 +0800

    feat: format text on about keystone widget

--- src/ui/gui_widgets/gui_about_keystone_widgets.c
+++ src/ui/gui_widgets/gui_about_keystone_widgets.c
@@ -12,19 +12,15 @@
 #include "gui_page.h"
 
 
-typedef struct {
+typedef struct
+{
     const char *url;
-    const char *tittle;
+    const char *title;
     const lv_img_dsc_t *icon;
     const lv_img_dsc_t *qrIcon;
 } ContactItem_t;
 
-static ContactItem_t g_contactItems[] = {
-    {"keyst.one", "WebSite", &imgNetwork, &imgQrcodeTurquoise},
-    {"twitter.com/KeystoneWallet", "Twitter", &imgTwitter, &imgQrcodeTurquoise},
-    {"keyst.one/discord", "Discord", &imgDiscord, &imgQrcodeTurquoise},
-    {"t.me/keystonewallet", "Telegram", &imgTelegram, &imgQrcodeTurquoise},
-};
+static ContactItem_t *g_contactItems = NULL;
 
 static lv_obj_t *g_cont;
 static lv_obj_t *g_qrCodeCont;
@@ -35,8 +31,36 @@ static void GuiAboutKeystoneEntranceWidget(lv_obj_t *parent);
 static void ShowQRDialogHandler(lv_event_t *e);
 static void GuiCloseQrcodeHandler(lv_event_t *e);
 
+void ContactItemsInit()
+{
+    if(g_contactItems == NULL)
+    {
+        g_contactItems = SRAM_MALLOC(4 * sizeof(ContactItem_t));
+        g_contactItems[0].url = _("about_keystone_website_url");
+        g_contactItems[0].title = _("about_keystone_website");
+        g_contactItems[0].icon = &imgNetwork;
+        g_contactItems[0].qrIcon = &imgQrcodeTurquoise;
+
+        g_contactItems[1].url = _("about_keystone_twitter_url");
+        g_contactItems[1].title = _("about_keystone_twitter");
+        g_contactItems[1].icon = &imgTwitter;
+        g_contactItems[1].qrIcon = &imgQrcodeTurquoise;
+
+        g_contactItems[2].url = _("about_keystone_discord_url");
+        g_contactItems[2].title = _("about_keystone_discord");
+        g_contactItems[2].icon = &imgDiscord;
+        g_contactItems[2].qrIcon = &imgQrcodeTurquoise;
+
+        g_contactItems[3].url = _("about_keystone_telegram_url");
+        g_contactItems[3].title = _("about_keystone_telegram");
+        g_contactItems[3].icon = &imgTelegram;
+        g_contactItems[3].qrIcon = &imgQrcodeTurquoise;
+    }
+}
+
 void GuiAboutKeystoneWidgetsInit()
 {
+    ContactItemsInit();
     g_pageWidget = CreatePageWidget();
     lv_obj_t *cont = g_pageWidget->contentZone;
 
@@ -46,11 +70,13 @@ void GuiAboutKeystoneWidgetsInit()
 
 void GuiAboutKeystoneWidgetsDeInit()
 {
-    if (g_cont != NULL) {
+    if (g_cont != NULL)
+    {
         lv_obj_del(g_cont);
         g_cont = NULL;
     }
-    if (g_pageWidget != NULL) {
+    if (g_pageWidget != NULL)
+    {
         DestroyPageWidget(g_pageWidget);
         g_pageWidget = NULL;
     }
@@ -69,7 +95,7 @@ void GuiAboutKeystoneWidgetsRestart()
 static void GuiAboutNVSBarInit()
 {
     SetNavBarLeftBtn(g_pageWidget->navBarWidget, NVS_BAR_RETURN, CloseCurrentViewHandler, NULL);
-    SetMidBtnLabel(g_pageWidget->navBarWidget, NVS_BAR_MID_LABEL, "About Keystone");
+    SetMidBtnLabel(g_pageWidget->navBarWidget, NVS_BAR_MID_LABEL, _("about_keystone_title"));
 }
 
 
@@ -77,12 +103,14 @@ void GuiAboutKeystoneEntranceWidget(lv_obj_t *parent)
 {
 
     lv_obj_t *imgIcon, *label, *imgQr;
-    for (int i = 0; i < NUMBER_OF_ARRAYS(g_contactItems); i++) {
+    for (int i = 0; i < NUMBER_OF_ARRAYS(g_contactItems); i++)
+    {
         imgIcon = GuiCreateImg(parent, g_contactItems[i].icon);
         label = GuiCreateLabel(parent, g_contactItems[i].url);
         imgQr = GuiCreateImg(parent, g_contactItems[i].qrIcon);
 
-        GuiButton_t table[] = {
+        GuiButton_t table[] =
+        {
             {
                 .obj = imgIcon,
                 .align = LV_ALIGN_LEFT_MID,
@@ -112,7 +140,8 @@ static void ShowQRDialogHandler(lv_event_t *e)
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t *parent, *button, *qrCodeCont, *qrCode, *label;
 
-    if (code == LV_EVENT_CLICKED) {
+    if (code == LV_EVENT_CLICKED)
+    {
         ContactItem_t *contactItem = (ContactItem_t*)lv_event_get_user_data(e);
 
         g_qrCodeCont = GuiCreateHintBox(g_cont, 480, 656, true);
@@ -133,13 +162,13 @@ static void ShowQRDialogHandler(lv_event_t *e)
         lv_obj_align(qrCode, LV_ALIGN_CENTER, 0, 0);
         lv_qrcode_update(qrCode, contactItem->url, (uint32_t)strlen(contactItem->url));
 
-        label = GuiCreateLittleTitleLabel(parent, contactItem->tittle);
+        label = GuiCreateLittleTitleLabel(parent, contactItem->title);
         lv_obj_align(label, LV_ALIGN_BOTTOM_LEFT, 36, -156);
         label = GuiCreateIllustrateLabel(parent, contactItem->url);
         lv_obj_set_style_text_color(label, lv_color_hex(0x1BE0C6), LV_PART_MAIN);
         lv_obj_align(label, LV_ALIGN_BOTTOM_LEFT, 36, -114);
 
-        button = GuiCreateBtn(parent, "OK");
+        button = GuiCreateBtn(parent, _("common_ok"));
         lv_obj_set_size(button, 94, 66);
         lv_obj_set_style_bg_color(button, WHITE_COLOR_OPA20, LV_PART_MAIN);
         lv_obj_align(button, LV_ALIGN_BOTTOM_RIGHT, -36, -24);
@@ -150,8 +179,10 @@ static void ShowQRDialogHandler(lv_event_t *e)
 static void GuiCloseQrcodeHandler(lv_event_t *e)
 {
     lv_event_code_t code = lv_event_get_code(e);
-    if (code == LV_EVENT_CLICKED) {
-        if (g_qrCodeCont != NULL) {
+    if (code == LV_EVENT_CLICKED)
+    {
+        if (g_qrCodeCont != NULL)
+        {
             lv_obj_del(g_qrCodeCont);
             g_qrCodeCont = NULL;
         }

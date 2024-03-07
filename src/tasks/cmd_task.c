7656e519d25478c5782555d857891587f2cff1b9
stone wang <15809188520@163.com>
2024-03-07 15:59:14 +0800

    fix build

--- src/tasks/cmd_task.c
+++ src/tasks/cmd_task.c
@@ -2,13 +2,11 @@
 #include "cmsis_os.h"
 #include "general_msg.h"
 #include "user_msg.h"
-#include "test_cmd.h"
 #include "mhscpu.h"
 
 #define TEST_CMD_MAX_LENGTH     3072
 
 static void CmdTask(void *pvParameter);
-
 
 uint8_t g_testCmdRcvBuffer[TEST_CMD_MAX_LENGTH];
 uint32_t g_testCmdRcvCount = 0;

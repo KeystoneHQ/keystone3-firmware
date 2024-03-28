8f6e79a1f5b199d0ddbc3dd548022c06386d08ae
soralit <soralitria@gmail.com>
2024-03-28 18:53:17 +0800

    feat(simulator): implement multisig import widgets

--- src/ui/gui_widgets/multi_sig/gui_multi_sig_import_wallet_info_widgets.h
+++ src/ui/gui_widgets/multi_sig/gui_multi_sig_import_wallet_info_widgets.h
@@ -1,6 +1,8 @@
 #ifndef _GUI_MULTI_SIG_IMPORT_WALLET_INFO_WIDGETS_H
 #define _GUI_MULTI_SIG_IMPORT_WALLET_INFO_WIDGETS_H
 
+void GuiSetMultisigImportWalletData(URParseResult *urResult, URParseMultiResult *multiResult, bool multi);
+
 void GuiImportWalletInfoWidgetsInit();
 void GuiImportWalletInfoWidgetsDeInit();
 void GuiImportWalletInfoWidgetsRefresh();

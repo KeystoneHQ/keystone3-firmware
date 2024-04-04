18fea712f3d0abaf0ad720a31d6ef8e476ab6a83
stone wang <15809188520@163.com>
2024-04-04 23:17:42 +0800

    add manage multisig view

---
+++ src/ui/gui_views/btc_only/multi_sig/gui_manage_multisig_wallet_view.c
@@ -0,0 +1,38 @@
+#include "gui.h"
+#include "gui_views.h"
+#include "gui_manage_multisig_wallet_widgets.h"
+
+int32_t GuiManageMultiViewEventProcess(void *self, uint16_t usEvent, void *param, uint16_t usLen)
+{
+    GUI_ASSERT(g_createMultisigWalletView.isActive);
+
+    switch (usEvent) {
+    case GUI_EVENT_OBJ_INIT:
+        GuiManageMultisigWalletInit();
+        break;
+    case GUI_EVENT_OBJ_DEINIT:
+        GuiManageMultisigWalletDeInit();
+        break;
+    case GUI_EVENT_REFRESH:
+        GuiManageMultisigWalletRefresh();
+        break;
+    case SIG_SETUP_VIEW_TILE_PREV:
+        GuiManageMultiWalletPrevTile();
+        break;
+    case SIG_SETUP_VIEW_TILE_NEXT:
+        GuiManageMultisigWalletNextTile(0xFF);
+        break;
+    default:
+        return ERR_GUI_UNHANDLED;
+    }
+    return SUCCESS_CODE;
+}
+
+GUI_VIEW g_manageMultisigWalletView = {
+    .id = SCREEN_MANAGE_MULTI_SIG,
+    .previous = NULL,
+    .isActive = false,
+    .optimization = false,
+    .pEvtHandler = GuiManageMultiViewEventProcess,
+};
+
---
+++ src/ui/gui_widgets/btc_only/multi_sig/gui_manage_multisig_wallet_widgets.h
@@ -0,0 +1,11 @@
+#ifndef _GUI_MANAGE_MULTI_SIG_WIDGETS_H
+#define _GUI_MANAGE_MULTI_SIG_WIDGETS_H
+
+void GuiManageMultisigWalletInit();
+void GuiManageMultisigWalletDeInit(void);
+void GuiManageMultisigWalletRefresh(void);
+int8_t GuiManageMultiWalletPrevTile(void);
+int8_t GuiManageMultisigWalletNextTile(uint8_t index);
+
+#endif /* _GUI_MANAGE_MULTI_SIG_WIDGETS_H */
+

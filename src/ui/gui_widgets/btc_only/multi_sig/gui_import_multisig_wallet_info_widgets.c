20768cb0d3efdbd87cdfb3c4e188c074fd9e81aa
soralit <soralitria@gmail.com>
2024-04-11 16:26:32 +0800

    refactor: use common error modal UI interface to show errors

--- src/ui/gui_widgets/btc_only/multi_sig/gui_import_multisig_wallet_info_widgets.c
+++ src/ui/gui_widgets/btc_only/multi_sig/gui_import_multisig_wallet_info_widgets.c
@@ -40,10 +40,6 @@ static lv_obj_t *g_confirmLabel;
 static void GuiConfirmHandler(lv_event_t *e);
 static void GuiVerifyPassword();
 static void GuiOnFailedHandler(lv_event_t *e);
-static void GuiShowWalletExisted();
-static void GuiShowInvalidWalletConfig();
-static void GuiShowInvalidQRCode();
-static void GuiShowInvalidWalletFile();
 static void prepareWalletByQRCode(void *);
 static void prepareWalletBySDCard(char *);
 static void processResult(Ptr_Response_MultiSigWallet result);
@@ -70,9 +66,9 @@ void GuiImportMultisigWalletInfoWidgetsInit(void)
     g_pageWidget = CreatePageWidget();
     if (g_wallet == NULL) {
         if (g_isQRCode) {
-            GuiShowInvalidQRCode();
+            GuiCreateErrorCodeWindow(ERR_INVALID_QRCODE, &g_noticeWindow);
         } else {
-            GuiShowInvalidWalletFile();
+            GuiCreateErrorCodeWindow(ERR_MULTISIG_INVALID_FILE, &g_noticeWindow);
         }
         return;
     }
@@ -117,7 +113,7 @@ void GuiImportMultisigWalletInfoVerifyPasswordSuccess(void)
     Response_MultiSigWallet *response = parse_and_verify_multisig_config(seed, len, g_wallet->config_text, mfp, 4);
     if (response->error_code != 0) {
         printf("errorMessage: %s\r\n", response->error_message);
-        GuiShowInvalidWalletConfig();
+        GuiCreateErrorCodeWindow(ERR_MULTISIG_WALLET_CONFIG_INVALID, &g_noticeWindow);
         free_MultiSigWallet(response->data);
         return;
     }
@@ -244,7 +240,7 @@ static void GuiConfirmHandler(lv_event_t *e)
         }
         MultiSigWalletItem_t *wallet = GetMultisigWalletByVerifyCode(g_wallet->verify_code);
         if (wallet != NULL) {
-            GuiShowWalletExisted();
+            GuiCreateErrorCodeWindow(ERR_MULTISIG_WALLET_EXIST, &g_noticeWindow);
             return;
         }
         GuiVerifyPassword();
@@ -257,87 +253,6 @@ static void GuiVerifyPassword()
     SetKeyboardWidgetSelf(g_keyboardWidget, &g_keyboardWidget);
     static uint16_t sig = SIG_MULTISIG_WALLET_IMPORT_VERIFY_PASSWORD;
     SetKeyboardWidgetSig(g_keyboardWidget, &sig);
-}
-
-static void GuiShowInvalidQRCode()
-{
-    g_noticeWindow = GuiCreateHintBox(lv_scr_act(), 480, 356, false);
-    lv_obj_t *img = GuiCreateImg(g_noticeWindow, &imgFailed);
-    lv_obj_align(img, LV_ALIGN_DEFAULT, 38, 492);
-
-    lv_obj_t *label = GuiCreateLittleTitleLabel(g_noticeWindow, _("scan_qr_code_error_invalid_qrcode"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 588);
-
-    label = GuiCreateIllustrateLabel(g_noticeWindow, _("scan_qr_code_error_invalid_qrcode_desc"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 640);
-
-    lv_obj_t *btn = GuiCreateBtnWithFont(g_noticeWindow, _("OK"), g_defTextFont);
-    lv_obj_set_size(btn, 94, 66);
-    lv_obj_set_style_bg_color(btn, WHITE_COLOR, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(btn, LV_OPA_20, LV_PART_MAIN);
-    lv_obj_align(btn, LV_ALIGN_BOTTOM_RIGHT, -16, -24);
-    lv_obj_add_event_cb(btn, GuiOnFailedHandler, LV_EVENT_CLICKED, NULL);
-}
-
-static void GuiShowInvalidWalletFile()
-{
-    g_noticeWindow = GuiCreateHintBox(lv_scr_act(), 480, 356, false);
-    lv_obj_t *img = GuiCreateImg(g_noticeWindow, &imgFailed);
-    lv_obj_align(img, LV_ALIGN_DEFAULT, 38, 492);
-
-    lv_obj_t *label = GuiCreateLittleTitleLabel(g_noticeWindow, _("scan_qr_code_error_invalid_wallet_file"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 588);
-
-    label = GuiCreateIllustrateLabel(g_noticeWindow, _("scan_qr_code_error_invalid_wallet_file_desc"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 640);
-
-    lv_obj_t *btn = GuiCreateBtnWithFont(g_noticeWindow, _("OK"), g_defTextFont);
-    lv_obj_set_size(btn, 94, 66);
-    lv_obj_set_style_bg_color(btn, WHITE_COLOR, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(btn, LV_OPA_20, LV_PART_MAIN);
-    lv_obj_align(btn, LV_ALIGN_BOTTOM_RIGHT, -16, -24);
-    lv_obj_add_event_cb(btn, GuiOnFailedHandler, LV_EVENT_CLICKED, NULL);
-}
-
-static void GuiShowInvalidWalletConfig()
-{
-    g_noticeWindow = GuiCreateHintBox(lv_scr_act(), 480, 356, false);
-    lv_obj_t *img = GuiCreateImg(g_noticeWindow, &imgFailed);
-    lv_obj_align(img, LV_ALIGN_DEFAULT, 38, 492);
-
-    lv_obj_t *label = GuiCreateLittleTitleLabel(g_noticeWindow, _("multisig_import_wallet_invalid"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 588);
-
-    label = GuiCreateIllustrateLabel(g_noticeWindow, _("multisig_import_wallet_invalid_desc"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 640);
-
-    lv_obj_t *btn = GuiCreateBtnWithFont(g_noticeWindow, _("OK"), g_defTextFont);
-    lv_obj_set_size(btn, 94, 66);
-    lv_obj_set_style_bg_color(btn, WHITE_COLOR, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(btn, LV_OPA_20, LV_PART_MAIN);
-    lv_obj_align(btn, LV_ALIGN_BOTTOM_RIGHT, -16, -24);
-    lv_obj_add_event_cb(btn, GuiOnFailedHandler, LV_EVENT_CLICKED, NULL);
-}
-
-static void GuiShowWalletExisted()
-{
-#define PLUS_PADDING +384
-    g_noticeWindow = GuiCreateHintBox(lv_scr_act(), 480, 416, false);
-    lv_obj_t *img = GuiCreateImg(g_noticeWindow, &imgFailed);
-    lv_obj_align(img, LV_ALIGN_TOP_LEFT, 36, 48 PLUS_PADDING);
-
-    lv_obj_t *label = GuiCreateLittleTitleLabel(g_noticeWindow, _("multisig_import_wallet_exist"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 144 PLUS_PADDING);
-
-    label = GuiCreateIllustrateLabel(g_noticeWindow, _("multisig_import_wallet_exist_desc"));
-    lv_obj_align(label, LV_ALIGN_DEFAULT, 36, 196 PLUS_PADDING);
-
-    lv_obj_t *btn = GuiCreateBtnWithFont(g_noticeWindow, _("OK"), g_defTextFont);
-    lv_obj_set_size(btn, 94, 66);
-    lv_obj_set_style_bg_color(btn, WHITE_COLOR, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(btn, LV_OPA_20, LV_PART_MAIN);
-    lv_obj_align(btn, LV_ALIGN_BOTTOM_RIGHT, -16, -24);
-    lv_obj_add_event_cb(btn, GuiOnFailedHandler, LV_EVENT_CLICKED, NULL);
 }
 
 static void GuiOnFailedHandler(lv_event_t *e)

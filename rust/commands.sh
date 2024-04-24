757300d24c0d27a367aa58d56b5ebb06cb64e818
ZhenQian <zhenqian@keyst.one>
2024-04-24 14:23:08 +0800

    commands.sh : add cargo update command

--- rust/commands.sh
+++ rust/commands.sh
@@ -24,12 +24,22 @@ test () {
     done
 }
 
+update () {
+    for dir in ${DIRS[@]}; do
+        echo "$dir ==> rust cargo update"
+        cd $CWD/$dir && cargo update
+    done
+}
+
 case $command in
     format)
         format
         ;;
     test)
         test
+        ;;
+    update)
+        update
         ;;
     *)
         echo "invalid command ${command}"

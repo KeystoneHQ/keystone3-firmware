2cebd8e0f61df41a02f1c82a8467419210c16858
ZhenQian <zhenqian@keyst.one>
2024-04-29 16:04:14 +0800

    fix: change thorwallet CryptoAccount to CryptoMultiAccounts

--- rust/rust_c/src/wallet/src/multi_coins_wallet/src/thorwallet.rs
+++ rust/rust_c/src/wallet/src/multi_coins_wallet/src/thorwallet.rs
@@ -4,19 +4,24 @@ use alloc::format;
 use alloc::string::ToString;
 use common_rust_c::ffi::CSliceFFI;
 use common_rust_c::structs::ExtendedPublicKey;
-use common_rust_c::types::PtrT;
+use common_rust_c::types::{PtrString, PtrT};
 use common_rust_c::ur::{UREncodeResult, FRAGMENT_MAX_LENGTH_DEFAULT};
 use common_rust_c::utils::{recover_c_array, recover_c_char};
 use core::slice;
-use third_party::ur_registry::crypto_account::CryptoAccount;
 use third_party::ur_registry::error::URError;
+use third_party::ur_registry::extend::crypto_multi_accounts::CryptoMultiAccounts;
 use third_party::ur_registry::traits::RegistryItem;
+
+use crate::utils::normalize_xpub;
 
 #[no_mangle]
 pub extern "C" fn get_connect_thor_wallet_ur(
     master_fingerprint: *mut u8,
     length: u32,
+    serial_number: PtrString,
     public_keys: PtrT<CSliceFFI<ExtendedPublicKey>>,
+    device_type: PtrString,
+    device_version: PtrString,
 ) -> *mut UREncodeResult {
     if length != 4 {
         return UREncodeResult::from(URError::UrEncodeError(format!(
@@ -28,6 +33,9 @@ pub extern "C" fn get_connect_thor_wallet_ur(
     unsafe {
         let mfp = slice::from_raw_parts(master_fingerprint, length as usize);
         let keys = recover_c_array(public_keys);
+        let serial_number = recover_c_char(serial_number);
+        let device_type = recover_c_char(device_type);
+        let device_version = recover_c_char(device_version);
         let key1 = keys.get(0);
         let key2 = keys.get(1);
         let key3 = keys.get(2);
@@ -36,30 +44,7 @@ pub extern "C" fn get_connect_thor_wallet_ur(
         return if let (Some(k1), Some(k2), Some(k3), Some(k4), Some(k5)) =
             (key1, key2, key3, key4, key5)
         {
-            let k1_x_pub = recover_c_char(k1.xpub);
-            let k1_x_pub_path = recover_c_char(k1.path);
-            let k2_x_pub = recover_c_char(k2.xpub);
-            let k2_x_pub_path = recover_c_char(k2.path);
-            let k3_x_pub = recover_c_char(k3.xpub);
-            let k3_x_pub_path = recover_c_char(k3.path);
-            let k4_x_pub = recover_c_char(k4.xpub);
-            let k4_x_pub_path = recover_c_char(k4.path);
-            let k5_x_pub = recover_c_char(k5.xpub);
-            let k5_x_pub_path = recover_c_char(k5.path);
-            let extended_public_keys = [
-                k1_x_pub.trim(),
-                k2_x_pub.trim(),
-                k3_x_pub.trim(),
-                k4_x_pub.trim(),
-                k5_x_pub.trim(),
-            ];
-            let extend_public_key_paths = [
-                k1_x_pub_path.trim(),
-                k2_x_pub_path.trim(),
-                k3_x_pub_path.trim(),
-                k4_x_pub_path.trim(),
-                k5_x_pub_path.trim(),
-            ];
+
             let mfp = match <&[u8; 4]>::try_from(mfp) {
                 Ok(mfp) => mfp,
                 Err(e) => {
@@ -67,16 +52,19 @@ pub extern "C" fn get_connect_thor_wallet_ur(
                 }
             };
 
-            let result = app_wallets::blue_wallet::generate_crypto_account(
-                mfp,
-                &extended_public_keys,
-                &extend_public_key_paths,
+            let keys = normalize_xpub(keys).unwrap();
+            let result = app_wallets::thorwallet::generate_crypto_multi_accounts(
+                *mfp,
+                &serial_number,
+                keys,
+                &device_type,
+                &device_version,
             );
             match result.map(|v| v.try_into()) {
                 Ok(v) => match v {
                     Ok(data) => UREncodeResult::encode(
                         data,
-                        CryptoAccount::get_registry_type().get_type(),
+                        CryptoMultiAccounts::get_registry_type().get_type(),
                         FRAGMENT_MAX_LENGTH_DEFAULT.clone(),
                     )
                     .c_ptr(),

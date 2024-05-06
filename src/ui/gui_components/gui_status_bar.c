f89dd3a1d9952c5250297e719d4a893c36a94172
stone wang <15809188520@163.com>
2024-05-06 10:27:05 +0800

    fix btc only build

--- src/ui/gui_components/gui_status_bar.c
+++ src/ui/gui_components/gui_status_bar.c
@@ -309,6 +309,15 @@ void GuiStatusBarSetTestNet(void) {
   }
   RefreshStatusBar();
 }
+#else
+char *GetWalletNameByIndex(WALLET_LIST_INDEX_ENUM index) {
+  if (index == WALLET_LIST_ETERNL) {
+    return "Eternl";
+  } else if (index == WALLET_LIST_TYPHON) {
+    return "Typhon";
+  }
+  return g_walletBtn[index].name;
+}
 #endif
 
 void GuiStatusBarSetBattery(uint8_t percent, bool charging) {
@@ -607,15 +616,6 @@ void SetCoinWallet(NavBarWidget_t *navBarWidget, GuiChainCoinType index,
   navBarWidget->midBtn = GuiUpdateStatusCoinButton(
       navBarWidget->midBtn, (name != NULL) ? name : g_coinWalletBtn[index].name,
       g_coinWalletBtn[index].icon);
-}
-
-char *GetWalletNameByIndex(WALLET_LIST_INDEX_ENUM index) {
-  if (index == WALLET_LIST_ETERNL) {
-    return "Eternl";
-  } else if (index == WALLET_LIST_TYPHON) {
-    return "Typhon";
-  }
-  return g_walletBtn[index].name;
 }
 
 void SetWallet(NavBarWidget_t *navBarWidget, WALLET_LIST_INDEX_ENUM index,

d582e17b7f1a4c44d109b0fb506419132d9c9727
Charon-Fan <Charon.Dian@gmail.com>
2024-05-10 17:09:03 +1000

    fix: resolve empty password display issue in bip39

--- src/crypto/account_public_info.c
+++ src/crypto/account_public_info.c
@@ -540,7 +540,7 @@ int32_t TempAccountPublicInfo(uint8_t accountIndex, const char *password, bool s
         // g_accountPublicKey stores the current temp public key.
         printf("g_accountPublicKey stores the current temp public key.\r\n");
     } else {
-        SetIsTempAccount(true);
+        SetIsTempAccount(password != NULL && password[0] != 0);
         GuiApiEmitSignal(SIG_START_GENERATE_XPUB, NULL, 0);
         char* icarusMasterKey = NULL;
         FreePublicKeyRam();

c1f265dadfb8f3ea82ef1e49c0db706fbcd01ecc
Charon-Fan <Charon.Dian@gmail.com>
2024-05-11 18:31:32 +1000

    fix: tempaccount display issue for AR

--- src/crypto/account_public_info.c
+++ src/crypto/account_public_info.c
@@ -535,13 +535,12 @@ int32_t TempAccountPublicInfo(uint8_t accountIndex, const char *password, bool s
     bool isSlip39 = GetMnemonicType() == MNEMONIC_TYPE_SLIP39;
     int len = isSlip39 ? GetCurrentAccountEntropyLen() : sizeof(seed) ;
 
+    char *passphrase = GetPassphrase(accountIndex);
+    SetIsTempAccount(passphrase != NULL && passphrase[0] != 0);
     if (g_tempPublicKeyAccountIndex == accountIndex && set == false) {
-        SetIsTempAccount(false);
         // g_accountPublicKey stores the current temp public key.
         printf("g_accountPublicKey stores the current temp public key.\r\n");
     } else {
-        char *passphrase = GetPassphrase(accountIndex);
-        SetIsTempAccount(passphrase != NULL && passphrase[0] != 0);
         GuiApiEmitSignal(SIG_START_GENERATE_XPUB, NULL, 0);
         char* icarusMasterKey = NULL;
         FreePublicKeyRam();

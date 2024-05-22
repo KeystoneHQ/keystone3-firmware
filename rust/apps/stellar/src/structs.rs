e4e08ec281f48e10498757255ec477ad783d2c6f
Charon-Fan <Charon.Dian@gmail.com>
2024-05-22 16:37:46 +0700

    feat: Add enum for Stellar network types

---
+++ rust/apps/stellar/src/structs.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Networks {
+    Public = "Public Global Stellar Network ; September 2015",
+    Testnet = "Test SDF Network ; September 2015",
+    Futurenet = "Test SDF Future Network ; October 2022",
+    Sandbox = "Local Sandbox Stellar Network ; September 2022",
+    Standalone = "Standalone Network ; February 2017",
+}

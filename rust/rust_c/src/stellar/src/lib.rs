fd994512cab9b1bfed80c5c086c6b6b3b6520119
Charon-Fan <Charon.Dian@gmail.com>
2024-05-31 14:12:39 +0700

    feat: Update Stellar integration

--- rust/rust_c/src/stellar/src/lib.rs
+++ rust/rust_c/src/stellar/src/lib.rs
@@ -2,11 +2,13 @@
 
 extern crate alloc;
 use alloc::boxed::Box;
+use alloc::string::{String, ToString};
 use alloc::{format, slice};
 use cty::c_char;
 
 use crate::structs::DisplayStellarTx;
-use app_stellar::{address::get_address, base_to_xdr, strkeys::sign_signature_base};
+use app_stellar::strkeys::{sign_hash, sign_signature_base};
+use app_stellar::{address::get_address, base_to_xdr};
 use common_rust_c::errors::{ErrorCodes, RustCError};
 use common_rust_c::extract_ptr_with_type;
 use common_rust_c::structs::{SimpleResponse, TransactionCheckResult, TransactionParseResult};
@@ -14,7 +16,7 @@ use common_rust_c::types::{PtrBytes, PtrString, PtrT, PtrUR};
 use common_rust_c::ur::{UREncodeResult, FRAGMENT_MAX_LENGTH_DEFAULT};
 use common_rust_c::utils::{convert_c_char, recover_c_char};
 use third_party::hex;
-use third_party::ur_registry::stellar::stellar_sign_request::StellarSignRequest;
+use third_party::ur_registry::stellar::stellar_sign_request::{SignType, StellarSignRequest};
 use third_party::ur_registry::stellar::stellar_signature::StellarSignature;
 use third_party::ur_registry::traits::{RegistryItem, To};
 
@@ -73,13 +75,30 @@ pub extern "C" fn stellar_sign(ptr: PtrUR, seed: PtrBytes, seed_len: u32) -> Ptr
     let sign_request = extract_ptr_with_type!(ptr, StellarSignRequest);
     let sign_data = sign_request.get_sign_data();
     let path = sign_request.get_derivation_path().get_path().unwrap();
-    match sign_signature_base(&sign_data, &seed, &path) {
-        Ok(signature) => UREncodeResult::encode(
-            signature.to_vec(),
-            StellarSignature::get_registry_type().get_type(),
-            FRAGMENT_MAX_LENGTH_DEFAULT.clone(),
-        )
-        .c_ptr(),
-        Err(e) => UREncodeResult::from(e).c_ptr(),
+    match sign_request.get_sign_type() {
+        SignType::Transaction => match sign_signature_base(&sign_data, &seed, &path) {
+            Ok(signature) => UREncodeResult::encode(
+                signature.to_vec(),
+                StellarSignature::get_registry_type().get_type(),
+                FRAGMENT_MAX_LENGTH_DEFAULT.clone(),
+            )
+            .c_ptr(),
+            Err(e) => UREncodeResult::from(e).c_ptr(),
+        },
+        SignType::TransactionHash => match sign_hash(&sign_data, &seed, &path) {
+            Ok(signature) => UREncodeResult::encode(
+                signature.to_vec(),
+                StellarSignature::get_registry_type().get_type(),
+                FRAGMENT_MAX_LENGTH_DEFAULT.clone(),
+            )
+            .c_ptr(),
+            Err(e) => UREncodeResult::from(e).c_ptr(),
+        },
+        _ => {
+            return UREncodeResult::from(RustCError::UnsupportedTransaction(
+                "Transaction".to_string(),
+            ))
+            .c_ptr();
+        }
     }
 }

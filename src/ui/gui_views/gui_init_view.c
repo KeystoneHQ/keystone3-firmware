f955d00c4870d8f35b1723d1e8e2c47b05b07432
stone wang <15809188520@163.com>
2024-05-31 09:04:44 +0800

    fixed the shutdown option not being enabled in Japanese

--- src/ui/gui_views/gui_init_view.c
+++ src/ui/gui_views/gui_init_view.c
@@ -107,9 +107,7 @@ int32_t GUI_InitViewEventProcess(void *self, uint16_t usEvent, void *param, uint
     case SIG_INIT_POWER_OPTION:
         rcvValue = *(uint32_t *)param;
         if (rcvValue != 0) {
-            if (lv_anim_count_running() == 0) {
-                OpenMsgBox(&g_guiMsgBoxPowerOption);
-            }
+            OpenMsgBox(&g_guiMsgBoxPowerOption);
         } else {
             CloseMsgBox(&g_guiMsgBoxPowerOption);
         }

f97598e5ab697991db3b7b08771c8aaff461a554
soralit <soralitria@gmail.com>
2024-06-06 10:59:40 +0800

    fix(ton): check battery when create ton mnemonic

--- src/ui/gui_widgets/gui_create_wallet_widgets.c
+++ src/ui/gui_widgets/gui_create_wallet_widgets.c
@@ -673,7 +673,7 @@ static void TonPhraseButtonHandler(lv_event_t *e)
     lv_obj_align(container, LV_ALIGN_CENTER, 0, 0);
 
     img = GuiCreateImg(container, &imgBlueInformation);
-    lv_obj_align(img, LV_ALIGN_TOP_LEFT, 168, 24);
+    lv_obj_align(img, LV_ALIGN_TOP_LEFT, 168, 12);
 
     label = GuiCreateTextLabel(container, _("create_ton_wallet"));
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 90);
@@ -686,6 +686,11 @@ static void TonPhraseButtonHandler(lv_event_t *e)
     btn = GuiCreateBtn(container, _("Next"));
     lv_obj_set_size(btn, 408, 66);
     lv_obj_align(btn, LV_ALIGN_BOTTOM_MID, 0, -36);
+    if (GetBatterPercent() < 60) {
+        lv_obj_clear_flag(btn, LV_OBJ_FLAG_CLICKABLE);
+        lv_obj_set_style_bg_opa(btn, LV_OPA_30, LV_PART_MAIN);
+    }
+
     lv_obj_add_event_cb(btn, TonNewMnemonicHandler, LV_EVENT_CLICKED, NULL);
 }
 

28666580958d94dcbc29da65564ebbbc20a8d11e
stone wang <15809188520@163.com>
2024-06-13 10:19:20 +0800

    update

--- src/tasks/fetch_sensitive_data_task.c
+++ src/tasks/fetch_sensitive_data_task.c
@@ -16,7 +16,6 @@
 #include "usb_task.h"
 #include "anti_tamper.h"
 #include "device_setting.h"
-#include "background_task.h"
 
 static void FetchSensitiveDataTask(void *argument);
 
@@ -27,72 +26,72 @@ void CreateFetchSensitiveDataTask(void)
     const osThreadAttr_t SensitiveDataTask_attributes = {
         .name = "SensitiveDataTask",
         .stack_size = 1024 * 35,
-        .priority = (osPriority_t)osPriorityNormal,
+        .priority = (osPriority_t)osPriorityBelowNormal,
     };
     g_sensitiveDataTaskHandle = osThreadNew(FetchSensitiveDataTask, NULL, &SensitiveDataTask_attributes);
 }
 
-// /// @brief Notify the background task to execute a function.
-// /// @param[in] func The function which would be executed in background task.
-// /// @param[in] inData.
-// /// @param[in] inDataLen.
-// /// @return err code.
-// int32_t AsyncExecute(BackgroundAsyncFunc_t func, const void *inData, uint32_t inDataLen)
-// {
-//     BackgroundAsync_t async = {0};
-//     async.func = func;
-//     if (inDataLen > 0) {
-//         async.inData = SRAM_MALLOC(inDataLen);
-//         memcpy(async.inData, inData, inDataLen);
-//         async.inDataLen = inDataLen;
-//     }
-//     PubBufferMsg(SENSITIVE_MSG_EXECUTE, &async, sizeof(BackgroundAsync_t));
-//     return SUCCESS_CODE;
-// }
+/// @brief Notify the background task to execute a function.
+/// @param[in] func The function which would be executed in background task.
+/// @param[in] inData.
+/// @param[in] inDataLen.
+/// @return err code.
+int32_t AsyncExecute(BackgroundAsyncFunc_t func, const void *inData, uint32_t inDataLen)
+{
+    BackgroundAsync_t async = {0};
+    async.func = func;
+    if (inDataLen > 0) {
+        async.inData = SRAM_MALLOC(inDataLen);
+        memcpy(async.inData, inData, inDataLen);
+        async.inDataLen = inDataLen;
+    }
+    PubBufferMsg(SENSITIVE_MSG_EXECUTE, &async, sizeof(BackgroundAsync_t));
+    return SUCCESS_CODE;
+}
 
-// int32_t AsyncExecuteWithPtr(BackgroundAsyncFunc_t func, const void *inData)
-// {
-//     BackgroundAsync_t async = {0};
-//     async.func = func;
-//     async.inData = (void *)inData;
-//     async.inDataLen = 4;
-//     PubBufferMsg(SENSITIVE_MSG_EXECUTE, &async, sizeof(BackgroundAsync_t));
-//     return SUCCESS_CODE;
-// }
+int32_t AsyncExecuteWithPtr(BackgroundAsyncFunc_t func, const void *inData)
+{
+    BackgroundAsync_t async = {0};
+    async.func = func;
+    async.inData = (void *)inData;
+    async.inDataLen = 4;
+    PubBufferMsg(SENSITIVE_MSG_EXECUTE, &async, sizeof(BackgroundAsync_t));
+    return SUCCESS_CODE;
+}
 
-// /// @brief Notify the background task to execute a function after a specified time delay.
-// /// @param[in] func The function which would be executed in background task.
-// /// @param[in] inData.
-// /// @param[in] inDataLen.
-// /// @param[in] delay millisecond.
-// /// @return err code.
-// int32_t AsyncDelayExecute(BackgroundAsyncFunc_t func, const void *inData, uint32_t inDataLen, uint32_t delay)
-// {
-//     BackgroundAsync_t async = {0};
-//     async.func = func;
-//     async.delay = delay;
-//     if (inDataLen > 0) {
-//         async.inData = SRAM_MALLOC(inDataLen);
-//         memcpy(async.inData, inData, inDataLen);
-//         async.inDataLen = inDataLen;
-//     }
-//     PubBufferMsg(SENSITIVE_MSG_EXECUTE, &async, sizeof(BackgroundAsync_t));
-//     return SUCCESS_CODE;
-// }
+/// @brief Notify the background task to execute a function after a specified time delay.
+/// @param[in] func The function which would be executed in background task.
+/// @param[in] inData.
+/// @param[in] inDataLen.
+/// @param[in] delay millisecond.
+/// @return err code.
+int32_t AsyncDelayExecute(BackgroundAsyncFunc_t func, const void *inData, uint32_t inDataLen, uint32_t delay)
+{
+    BackgroundAsync_t async = {0};
+    async.func = func;
+    async.delay = delay;
+    if (inDataLen > 0) {
+        async.inData = SRAM_MALLOC(inDataLen);
+        memcpy(async.inData, inData, inDataLen);
+        async.inDataLen = inDataLen;
+    }
+    PubBufferMsg(SENSITIVE_MSG_EXECUTE, &async, sizeof(BackgroundAsync_t));
+    return SUCCESS_CODE;
+}
 
-// int32_t AsyncExecuteRunnable(BackgroundAsyncFuncWithRunnable_t func, const void *inData, uint32_t inDataLen, BackgroundAsyncRunnable_t runnable)
-// {
-//     BackgroundRunnable_t async = {0};
-//     async.func = func;
-//     if (inDataLen > 0) {
-//         async.inData = SRAM_MALLOC(inDataLen);
-//         memcpy(async.inData, inData, inDataLen);
-//         async.inDataLen = inDataLen;
-//     }
-//     async.runnable = runnable;
-//     PubBufferMsg(SENSITIVE_MSG_EXECUTE_RUNNABLE, &async, sizeof(BackgroundRunnable_t));
-//     return SUCCESS_CODE;
-// }
+int32_t AsyncExecuteRunnable(BackgroundAsyncFuncWithRunnable_t func, const void *inData, uint32_t inDataLen, BackgroundAsyncRunnable_t runnable)
+{
+    BackgroundRunnable_t async = {0};
+    async.func = func;
+    if (inDataLen > 0) {
+        async.inData = SRAM_MALLOC(inDataLen);
+        memcpy(async.inData, inData, inDataLen);
+        async.inDataLen = inDataLen;
+    }
+    async.runnable = runnable;
+    PubBufferMsg(SENSITIVE_MSG_EXECUTE_RUNNABLE, &async, sizeof(BackgroundRunnable_t));
+    return SUCCESS_CODE;
+}
 
 static void FetchSensitiveDataTask(void *argument)
 {

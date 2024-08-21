41f9f99adfd688d1bdd07d5758a409c5545445e7
stone wang <15809188520@163.com>
2024-08-21 15:36:24 +0800

    1. battery percent fix 2. run font script

--- src/ui/gui_assets/font/de/deText.c
+++ src/ui/gui_assets/font/de/deText.c
@@ -225,7 +225,7 @@ static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
     0x0, 0x0, 0x0, 0x0, 0x0, 0x1e, 0x0, 0x1,
     0xfd, 0x0, 0x2f, 0xd0, 0x2, 0xf9, 0x0, 0x2f,
     0x80, 0x0, 0xfc, 0x0, 0x0, 0x7f, 0x80, 0x0,
-    0x6, 0xf9, 0x0, 0x0, 0x2f, 0xd0, 0x0, 0x1,
+    0x7, 0xf9, 0x0, 0x0, 0x2f, 0xd0, 0x0, 0x1,
     0xfd, 0x0, 0x0, 0x1e, 0x0, 0x0, 0x0,
 
     /* U+003D "=" */
@@ -1001,8 +1001,7 @@ static const uint16_t unicode_list_1[] = {
 };
 
 /*Collect the unicode lists and glyph_id offsets*/
-static const lv_font_fmt_txt_cmap_t cmaps[] =
-{
+static const lv_font_fmt_txt_cmap_t cmaps[] = {
     {
         .range_start = 32, .range_length = 95, .glyph_id_start = 1,
         .unicode_list = NULL, .glyph_id_ofs_list = NULL, .list_length = 0, .type = LV_FONT_FMT_TXT_CMAP_FORMAT0_TINY
@@ -1019,12 +1018,9 @@ static const lv_font_fmt_txt_cmap_t cmaps[] =
  *  ALL CUSTOM DATA
  *--------------------*/
 
-#if LVGL_VERSION_MAJOR == 8
+#if LV_VERSION_CHECK(8, 0, 0)
 /*Store all the custom data of the font*/
 static  lv_font_fmt_txt_glyph_cache_t cache;
-#endif
-
-#if LVGL_VERSION_MAJOR >= 8
 static const lv_font_fmt_txt_dsc_t font_dsc = {
 #else
 static lv_font_fmt_txt_dsc_t font_dsc = {
@@ -1038,11 +1034,10 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
     .bpp = 2,
     .kern_classes = 0,
     .bitmap_format = 0,
-#if LVGL_VERSION_MAJOR == 8
+#if LV_VERSION_CHECK(8, 0, 0)
     .cache = &cache
 #endif
 };
-
 
 
 /*-----------------
@@ -1050,7 +1045,7 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
  *----------------*/
 
 /*Initialize a public general font descriptor*/
-#if LVGL_VERSION_MAJOR >= 8
+#if LV_VERSION_CHECK(8, 0, 0)
 const lv_font_t deText = {
 #else
 lv_font_t deText = {
@@ -1066,11 +1061,7 @@ lv_font_t deText = {
     .underline_position = -2,
     .underline_thickness = 1,
 #endif
-    .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
-#if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
-    .fallback = NULL,
-#endif
-    .user_data = NULL,
+    .dsc = &font_dsc           /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 };
 
 

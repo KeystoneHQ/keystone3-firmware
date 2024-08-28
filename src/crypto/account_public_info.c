50e536f62bb56fb3610843e0bac4a87f225abcf4
stone wang <15809188520@163.com>
2024-08-28 16:29:16 +0800

    fix:clear passphrase temp account info when switch wallet

--- src/crypto/account_public_info.c
+++ src/crypto/account_public_info.c
@@ -709,6 +709,11 @@ int32_t TempAccountPublicInfo(uint8_t accountIndex, const char *password, bool s
         GuiApiEmitSignal(SIG_END_GENERATE_XPUB, NULL, 0);
     }
     CLEAR_ARRAY(seed);
+
+    if (g_tempParsePhraseJson != NULL) {
+        cJSON_Delete(g_tempParsePhraseJson);
+        g_tempParsePhraseJson = NULL;
+    }
     return ret;
 }
 

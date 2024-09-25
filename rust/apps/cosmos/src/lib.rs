f05873fa90a07609466c4b330db225925b3ce8ff
ZhenQian <zhenqian@keyst.one>
2024-09-25 17:30:33 +0800

    fix: fix thorchain unit test case for old chain id

--- rust/apps/cosmos/src/lib.rs
+++ rust/apps/cosmos/src/lib.rs
@@ -111,14 +111,14 @@ mod tests {
 
     #[test]
     fn test_parse_tx() {
-        // transfer rune
+        // transfer rune old case
         let raw_tx = "0a570a500a0e2f74797065732e4d736753656e64123e0a14d2a392d2d0e98f64dd0f9aa422da9b37b21944501214ead5b280c71c6ae156ee581cff8c9147a64cca1f1a100a0472756e65120831303030303030301203626d79125a0a500a460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a2103b1f26f209231b1ee90a8c52a981038794195fd7e08c47df02ff8bb7c1ce2a43512040a020801180012061080cab5ee011a1474686f72636861696e2d6d61696e6e65742d763120bdb106";
         let data_type = transaction::structs::DataType::Direct;
         let parsed_tx = parse(&hex::decode(raw_tx).unwrap(), data_type).unwrap();
 
         let overview = parsed_tx.overview;
         let network = overview.common.network;
-        assert_eq!("THORChain", network.as_str(),)
+        assert_ne!("THORChain", network.as_str(),)
     }
 
     #[test]

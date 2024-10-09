af0e87fa3aa13e892504accd61afeeb3963b1a60
Charon-Fan <Charon.Dian@gmail.com>
2024-10-09 15:05:49 +0800

    chore: add test & fmt

--- rust/apps/monero/src/structs.rs
+++ rust/apps/monero/src/structs.rs
@@ -1,5 +1,13 @@
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub enum Network {
     Mainnet,
     Testnet,
     Stagenet,
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+
+pub enum AddressType {
+    Standard,
+    Subaddress,
 }

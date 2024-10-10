3c8483b2058e4f566d05b5b2cf70bcd2c33b67f5
Charon-Fan <Charon.Dian@gmail.com>
2024-10-10 09:39:42 +0800

    chore: monero_get_address

--- rust/rust_c/src/monero/src/lib.rs
+++ rust/rust_c/src/monero/src/lib.rs
@@ -1,3 +1,29 @@
 #![no_std]
 
 extern crate alloc;
+use alloc::string::ToString;
+use app_monero::address::Address;
+use app_monero::key::PublicKey;
+use app_monero::structs::{AddressType, Network};
+use common_rust_c::structs::SimpleResponse;
+use common_rust_c::types::PtrString;
+use common_rust_c::utils::{convert_c_char, recover_c_char};
+use cty::c_char;
+
+#[no_mangle]
+pub extern "C" fn monero_get_address(
+    pub_spend_key: PtrString,
+    pub_view_key: PtrString,
+) -> *mut SimpleResponse<c_char> {
+    let spend_key = recover_c_char(pub_spend_key);
+    let view_key = recover_c_char(pub_view_key);
+
+    let address = Address::new(
+        Network::Mainnet,
+        AddressType::Standard,
+        PublicKey::from_str(spend_key.as_str()).unwrap(),
+        PublicKey::from_str(view_key.as_str()).unwrap(),
+    );
+
+    SimpleResponse::success(convert_c_char(address.to_string()) as *mut c_char).simple_c_ptr()
+}

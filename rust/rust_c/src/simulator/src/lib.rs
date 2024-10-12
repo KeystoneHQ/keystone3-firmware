0c87b7cb2ec154b40f66c11b00804e96b81da2b5
soralit <soralitria@gmail.com>
2024-10-12 18:39:40 +0800

    style: run fmt

--- rust/rust_c/src/simulator/src/lib.rs
+++ rust/rust_c/src/simulator/src/lib.rs
@@ -7,7 +7,8 @@ pub extern "C" fn read_qr_code_from_screen(
     on_qr_detected: extern "C" fn(*const c_char) -> bool,
     max_loop_count: u32,
 ) {
-    let _ = continuously_read_qr_code_from_screen(|qr_code: &str| {
-        on_qr_detected(convert_c_char(qr_code.to_string()))
-    }, max_loop_count);
+    let _ = continuously_read_qr_code_from_screen(
+        |qr_code: &str| on_qr_detected(convert_c_char(qr_code.to_string())),
+        max_loop_count,
+    );
 }

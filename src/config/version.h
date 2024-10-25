d10ec9f3962b15be4594008b0b51e85586213c57
bianminyan <126669324+bianminyan@users.noreply.github.com>
2024-10-25 11:35:13 +0800

    Update version for test boot

--- src/config/version.h
+++ src/config/version.h
@@ -5,9 +5,9 @@
 #define SD_CARD_OTA_BIN_PATH                "0:/keystone3.bin"
 
 #define SOFTWARE_VERSION_MAX_LEN            (32)
-#define SOFTWARE_VERSION_MAJOR              999
-#define SOFTWARE_VERSION_MINOR              999
-#define SOFTWARE_VERSION_BUILD              999
+#define SOFTWARE_VERSION_MAJOR              99
+#define SOFTWARE_VERSION_MINOR              99
+#define SOFTWARE_VERSION_BUILD              99
 #define SOFTWARE_VERSION_BETA               0
 #define SOFTWARE_VERSION                    (SOFTWARE_VERSION_MAJOR * 10000 + SOFTWARE_VERSION_MINOR * 100 + SOFTWARE_VERSION_BUILD)
 

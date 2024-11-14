67d2546bdd9ded92d664fc2dcbf4264d729ae9bb
ZhenQian <zhenqian@keyst.one>
2024-11-14 13:13:45 +0800

    fix: add sui unit test case

--- rust/apps/sui/src/lib.rs
+++ rust/apps/sui/src/lib.rs
@@ -195,4 +195,19 @@ mod tests {
         let expected_signature = hex::decode("f4b79835417490958c72492723409289b444f3af18274ba484a9eeaca9e760520e453776e5975df058b537476932a45239685f694fc6362fe5af6ba714da6505").unwrap();
         assert_eq!(expected_signature, signature);
     }
+
+    #[test]
+    fn test_sign_message_hash() {
+        let seed = hex::decode("a33b2bdc2dc3c53d24081e5ed2273e6e8e0e43f8b26c746fbd1db2b8f1d4d8faa033545d3ec9303d36e743a4574b80b124353d380535532bb69455dc0ee442c4").unwrap();
+        let hd_path = "m/44'/784'/0'/0'/0'".to_string();
+        let msg =  hex::decode("00000000000200201ff915a5e9e32fdbe0135535b6c69a00a9809aaf7f7c0275d3239ca79db20d6400081027000000000000020200010101000101020000010000ebe623e33b7307f1350f8934beb3fb16baef0fc1b3f1b92868eec3944093886901a2e3e42930675d9571a467eb5d4b22553c93ccb84e9097972e02c490b4e7a22ab73200000000000020176c4727433105da34209f04ac3f22e192a2573d7948cb2fabde7d13a7f4f149ebe623e33b7307f1350f8934beb3fb16baef0fc1b3f1b92868eec39440938869e803000000000000640000000000000000").unwrap();
+        // msg_hash = blake2b(msg_bytes)
+        let mut hasher = Blake2bVar::new(32).unwrap();
+        hasher.update(&msg);
+        let mut hash = [0u8; 32];
+        hasher.finalize_variable(&mut hash).unwrap();
+        let signature = sign_hash(&seed, &hd_path, &hash).unwrap();
+        let expected_signature = hex::decode("f4b79835417490958c72492723409289b444f3af18274ba484a9eeaca9e760520e453776e5975df058b537476932a45239685f694fc6362fe5af6ba714da6505").unwrap();
+        assert_eq!(expected_signature, signature);
+    }
 }

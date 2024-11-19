c6e21ce752daf27dabd9180cd36b0b8937086526
soralit <soralitria@gmail.com>
2024-11-19 09:40:30 +0800

    feat: zcash ui

--- src/ui/gui_analyze/gui_analyze.c
+++ src/ui/gui_analyze/gui_analyze.c
@@ -298,6 +298,17 @@ const static GuiAnalyze_t g_analyzeArray[] = {
         NULL,
         FreeArMemory,
     },
+    {
+        REMAPVIEW_ZCASH,
+        #ifndef COMPILE_SIMULATOR
+        "{\"name\":\"ton_page\",\"type\":\"tabview\",\"pos\":[36,0],\"size\":[408,900],\"bg_color\":0,\"children\":[{\"type\":\"tabview_child\",\"index\":1,\"tab_name\":\"Overview\",\"font\":\"openSansEnIllustrate\",\"children\":[{\"type\":\"custom_container\",\"bg_color\":0,\"bg_opa\":0,\"pos\":[0,12],\"custom_show_func\":\"GuiTonTxOverview\"}]},{\"type\":\"tabview_child\",\"index\":2,\"tab_name\":\"Raw Data\",\"text_color\":16777215,\"font\":\"openSansEnIllustrate\",\"children\":[{\"type\":\"custom_container\",\"bg_color\":0,\"bg_opa\":0,\"pos\":[0,12],\"custom_show_func\":\"GuiTonTxRawData\"}]}]}",
+#else
+        PC_SIMULATOR_PATH "/page_zcash.json",
+#endif
+        GuiGetTonProofGUIData,
+        NULL,
+        FreeArMemory,
+    }
 #endif
 };
 

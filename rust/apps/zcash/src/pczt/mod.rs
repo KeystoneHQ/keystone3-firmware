e1425b6d87e0be5418048437d660566e850b9427
soralit <soralitria@gmail.com>
2024-12-16 16:10:12 +0800

    fix: unshielding transaction failed because of the hash calculation

--- rust/apps/zcash/src/pczt/mod.rs
+++ rust/apps/zcash/src/pczt/mod.rs
@@ -4,11 +4,6 @@ use keystore::algorithms::secp256k1::{get_public_key_by_seed, sign_message_by_se
 use keystore::algorithms::zcash::{calculate_seed_fingerprint, sign_message_orchard};
 use zcash_vendor::{
     orchard,
-    pczt::{
-        common::Zip32Derivation,
-        pczt_ext::{PcztSigner, ZcashSignature},
-        Pczt,
-    },
     zcash_keys::keys::UnifiedFullViewingKey,
 };
 

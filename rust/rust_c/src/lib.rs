d4452ee09eed3a687f753661ce1954fd832c4e09
soralit <soralitria@gmail.com>
2024-12-24 16:22:38 +0800

    chore: organize rust_c features

--- rust/rust_c/src/lib.rs
+++ rust/rust_c/src/lib.rs
@@ -12,54 +12,58 @@ mod allocator;
 mod my_alloc;
 
 #[allow(unused)]
-use bitcoin_rust_c;
-#[allow(unused)]
 use common_rust_c;
-#[allow(unused)]
-use test_cmd;
 #[allow(unused)]
 use wallet_rust_c;
 
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "test_cmd")]
+#[allow(unused)]
+use test_cmd;
+
+//chains
+#[cfg(feature = "aptos")]
 #[allow(unused)]
 use aptos_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "arweave")]
 #[allow(unused)]
 use arweave_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "bitcoin")]
+#[allow(unused)]
+use bitcoin_rust_c;
+#[cfg(feature = "cardano")]
 #[allow(unused)]
 use cardano_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "cosmos")]
 #[allow(unused)]
 use cosmos_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "ethereum")]
 #[allow(unused)]
 use ethereum_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "near")]
 #[allow(unused)]
 use near_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "solana")]
 #[allow(unused)]
 use solana_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "stellar")]
 #[allow(unused)]
 use stellar_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "sui")]
 #[allow(unused)]
 use sui_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "ton")]
 #[allow(unused)]
 use ton_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "tron")]
 #[allow(unused)]
 use tron_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "xrp")]
 #[allow(unused)]
 use xrp_rust_c;
-#[cfg(feature = "multi-coins")]
+#[cfg(feature = "zcash")]
 #[allow(unused)]
 use zcash_rust_c;
 
-#[cfg(any(feature = "simulator", feature = "simulator_btc_only"))]
+#[cfg(feature = "simulator")]
 #[allow(unused)]
 use simulator_rust_c;

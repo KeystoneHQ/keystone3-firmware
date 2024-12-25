6b5c429547539ee266c3ab0680b07a588711bda0
soralit <soralitria@gmail.com>
2024-12-25 17:26:36 +0800

    chore: refactor cbindgen

--- rust/rust_c/build.rs
+++ rust/rust_c/build.rs
@@ -1,65 +1,21 @@
 fn main() {
-    return;
-    let mut config = cbindgen::Config::default();
-    config.language = cbindgen::Language::C;
-    let includes = vec![
-        "common_rust_c",
-        "wallet_rust_c",
-        "btc_only_wallet_rust_c",
-        "multi_coins_wallet_rust_c",
-        #[cfg(feature = "aptos")]
-        "aptos_rust_c",
-        #[cfg(feature = "simulator")]
-        "simulator_rust_c",
-        #[cfg(feature = "arweave")]
-        "arweave_rust_c",
-        #[cfg(feature = "bitcoin")]
-        "bitcoin_rust_c",
-        #[cfg(feature = "cardano")]
-        "cardano_rust_c",
-        #[cfg(feature = "cosmos")]
-        "cosmos_rust_c",
-        #[cfg(feature = "ethereum")]
-        "ethereum_rust_c",
-        #[cfg(feature = "near")]
-        "near_rust_c",
-        #[cfg(feature = "solana")]
-        "solana_rust_c",
-        #[cfg(feature = "stellar")]
-        "stellar_rust_c",
-        #[cfg(feature = "sui")]
-        "sui_rust_c",
-        #[cfg(feature = "ton")]
-        "ton_rust_c",
-        #[cfg(feature = "tron")]
-        "tron_rust_c",
-        #[cfg(feature = "xrp")]
-        "xrp_rust_c",
-        #[cfg(feature = "zcash")]
-        "zcash_rust_c",
-        #[cfg(feature = "test_cmd")]
-        "test_cmd_rust_c",
-        #[cfg(feature = "test_cmd")]
-        "btc_test_cmd_rust_c",
-        #[cfg(feature = "test_cmd")]
-        "general_test_cmd_rust_c",
-    ];
-
-    let crates = includes
-        .into_iter()
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>();
-
-    config.parse.include = Some(crates.clone());
-    config.parse.parse_deps = true;
-    config.parse.expand.crates = crates;
+    let mut config =
+        cbindgen::Config::from_file("cbindgen.toml").expect("Failed to read cbindgen.toml");
     let features = vec![
-        #[cfg(feature = "btc-only")]
-        "btc_only",
-        #[cfg(feature = "multi-coins")]
-        "multi_coins",
+        #[cfg(feature = "production-multi-coins")]
+        "production-multi-coins",
+        #[cfg(feature = "production-btc-only")]
+        "production-btc-only",
+        #[cfg(feature = "simulator-multi-coins")]
+        "simulator-multi-coins",
+        #[cfg(feature = "simulator-btc-only")]
+        "simulator-btc-only",
+        #[cfg(feature = "debug-multi-coins")]
+        "debug-multi-coins",
+        #[cfg(feature = "debug-btc-only")]
+        "debug-btc-only",
     ];
-
+    println!("cargo:warning={}", format!("features: {:?}", features));
     config.parse.expand.features = Some(features.into_iter().map(|s| s.to_string()).collect());
 
     let builder = cbindgen::Builder::new();

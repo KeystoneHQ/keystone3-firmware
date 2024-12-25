0ecfcf08e94d7d1e23094a8279b97c3d86b98a06
soralit <soralitria@gmail.com>
2024-12-25 18:58:34 +0800

    fix: all build

--- rust/rust_c/src/zcash/mod.rs
+++ rust/rust_c/src/zcash/mod.rs
@@ -1,7 +1,5 @@
 pub mod structs;
 
-use alloc::{boxed::Box, format};
-use app_zcash::get_address;
 use crate::common::{
     errors::RustCError,
     free::Free,
@@ -10,10 +8,16 @@ use crate::common::{
     ur::{UREncodeResult, FRAGMENT_MAX_LENGTH_DEFAULT},
     utils::{convert_c_char, recover_c_char},
 };
-use crate::{extract_ptr_with_type, make_free_method, };
+use crate::{extract_ptr_with_type, make_free_method};
+use alloc::{boxed::Box, format, string::String, string::ToString};
+use app_zcash::get_address;
 use core::slice;
+use cryptoxide::hashing::sha256;
 use cty::c_char;
-use keystore::algorithms::zcash::{calculate_seed_fingerprint, derive_ufvk};
+use keystore::algorithms::{
+    secp256k1::get_private_key_by_seed,
+    zcash::{calculate_seed_fingerprint, derive_ufvk},
+};
 use structs::DisplayPczt;
 use ur_registry::{traits::RegistryItem, zcash::zcash_pczt::ZcashPczt};
 use zcash_vendor::zcash_protocol::consensus::MainNetwork;
@@ -125,3 +129,122 @@ pub extern "C" fn sign_zcash_tx(tx: PtrUR, seed: PtrBytes, seed_len: u32) -> *mu
 }
 
 make_free_method!(TransactionParseResult<DisplayPczt>);
+
+use aes::cipher::block_padding::Pkcs7;
+use aes::cipher::generic_array::GenericArray;
+use aes::cipher::{BlockDecryptMut, BlockEncryptMut, KeyIvInit};
+
+type Aes256CbcEnc = cbc::Encryptor<aes::Aes256>;
+type Aes256CbcDec = cbc::Decryptor<aes::Aes256>;
+
+#[no_mangle]
+pub extern "C" fn rust_aes256_cbc_encrypt(
+    data: PtrString,
+    password: PtrString,
+    iv: PtrBytes,
+    iv_len: u32,
+) -> *mut SimpleResponse<c_char> {
+    let data = recover_c_char(data);
+    let data = data.as_bytes();
+    let password = recover_c_char(password);
+    let iv = unsafe { slice::from_raw_parts(iv, iv_len as usize) };
+    let key = sha256(password.as_bytes());
+    let iv = GenericArray::from_slice(&iv);
+    let key = GenericArray::from_slice(&key);
+    let ct = Aes256CbcEnc::new(key, iv).encrypt_padded_vec_mut::<Pkcs7>(data);
+    SimpleResponse::success(convert_c_char(hex::encode(ct))).simple_c_ptr()
+}
+
+#[no_mangle]
+pub extern "C" fn rust_aes256_cbc_decrypt(
+    hex_data: PtrString,
+    password: PtrString,
+    iv: PtrBytes,
+    iv_len: u32,
+) -> *mut SimpleResponse<c_char> {
+    let hex_data = recover_c_char(hex_data);
+    let data = hex::decode(hex_data).unwrap();
+    let password = recover_c_char(password);
+    let iv = unsafe { slice::from_raw_parts(iv, iv_len as usize) };
+    let key = sha256(password.as_bytes());
+    let iv = GenericArray::from_slice(&iv);
+    let key = GenericArray::from_slice(&key);
+
+    match Aes256CbcDec::new(key, iv).decrypt_padded_vec_mut::<Pkcs7>(&data) {
+        Ok(pt) => {
+            SimpleResponse::success(convert_c_char(String::from_utf8(pt).unwrap())).simple_c_ptr()
+        }
+        Err(_e) => SimpleResponse::from(RustCError::InvalidHex("decrypt failed".to_string()))
+            .simple_c_ptr(),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn rust_derive_iv_from_seed(
+    seed: PtrBytes,
+    seed_len: u32,
+) -> *mut SimpleResponse<u8> {
+    let seed = unsafe { slice::from_raw_parts(seed, seed_len as usize) };
+    let iv_path = "m/44'/1557192335'/0'/2'/0'".to_string();
+    let iv = get_private_key_by_seed(seed, &iv_path).unwrap();
+    let mut iv_bytes = [0; 16];
+    iv_bytes.copy_from_slice(&iv[..16]);
+    SimpleResponse::success(Box::into_raw(Box::new(iv_bytes)) as *mut u8).simple_c_ptr()
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::{string::String, vec::Vec};
+
+    use super::*;
+
+    #[test]
+    fn test_aes256_cbc_encrypt() {
+        let mut data = convert_c_char("hello world".to_string());
+        let mut password = convert_c_char("password".to_string());
+        let mut seed = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+        let iv = rust_derive_iv_from_seed(seed.as_mut_ptr(), 64);
+        let mut iv = unsafe { slice::from_raw_parts_mut((*iv).data, 16) };
+        let iv_len = 16;
+        let ct = rust_aes256_cbc_encrypt(data, password, iv.as_mut_ptr(), iv_len as u32);
+        let ct_vec = unsafe { (*ct).data };
+        let value = recover_c_char(ct_vec);
+        assert_eq!(value, "639194f4bf964e15d8ea9c9bd9d96918");
+    }
+
+    #[test]
+    fn test_aes256_cbc_decrypt() {
+        let data = convert_c_char("639194f4bf964e15d8ea9c9bd9d96918".to_string());
+        let password = convert_c_char("password".to_string());
+        let mut seed = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+        let iv = rust_derive_iv_from_seed(seed.as_mut_ptr(), 64);
+        let iv = unsafe { slice::from_raw_parts_mut((*iv).data, 16) };
+        let iv_len = 16;
+        let pt = rust_aes256_cbc_decrypt(data, password, iv.as_mut_ptr(), iv_len as u32);
+        assert!(!pt.is_null());
+        let ct_vec = unsafe { (*pt).data };
+        let value = recover_c_char(ct_vec);
+        assert_eq!(value, "hello world");
+    }
+
+    #[test]
+    fn test_dep_aes256() {
+        let mut data = b"hello world";
+        let seed = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+        let iv_path = "m/44'/1557192335'/0'/2'/0'".to_string();
+        let iv = get_private_key_by_seed(&seed, &iv_path).unwrap();
+        let mut iv_bytes = [0; 16];
+        iv_bytes.copy_from_slice(&iv[..16]);
+        let key = sha256(b"password");
+        let iv = GenericArray::from_slice(&iv_bytes);
+        let key = GenericArray::from_slice(&key);
+
+        let encrypter = Aes256CbcEnc::new(key, iv);
+        let decrypter = Aes256CbcDec::new(key, iv);
+
+        let ct = encrypter.encrypt_padded_vec_mut::<Pkcs7>(data);
+        let pt = decrypter.decrypt_padded_vec_mut::<Pkcs7>(&ct).unwrap();
+
+        assert_eq!(String::from_utf8(pt).unwrap(), "hello world");
+    }
+}

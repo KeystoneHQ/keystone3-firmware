1d056bf1beb55108dd8972b306c59c33262afd19
bianminyan <bianminyan@163.com>
2025-01-08 17:14:40 +0800

    change beta version

--- src/config/version_cypherpunk.h
+++ src/config/version_cypherpunk.h
@@ -1,8 +1,8 @@
-#define SOFTWARE_VERSION_MAJOR              9 // 1 + 8
+#define SOFTWARE_VERSION_MAJOR              8 // 1 + 8
 #define SOFTWARE_VERSION_MAJOR_OFFSET       8
-#define SOFTWARE_VERSION_MINOR              0
+#define SOFTWARE_VERSION_MINOR              9
 #define SOFTWARE_VERSION_BUILD              0
-#define SOFTWARE_VERSION_BETA               0
+#define SOFTWARE_VERSION_BETA               1
 #define SOFTWARE_VERSION                    (SOFTWARE_VERSION_MAJOR * 10000 + SOFTWARE_VERSION_MINOR * 100 + SOFTWARE_VERSION_BUILD)
 #define SOFTWARE_VERSION_SUFFIX             "Cypherpunk"
 

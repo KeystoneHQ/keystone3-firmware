a2cc170011d5801c8c81d99329365ad894140046
ww3512687 <15809188520@163.com>
2025-01-21 10:37:51 +0800

    add boot.sig

--- tools/padding_bin_file/padding_bin_file.py
+++ tools/padding_bin_file/padding_bin_file.py
@@ -8,7 +8,18 @@ def padding_bin_file(file_name):
         num = current_size % 4096
         f.write(num.to_bytes(2, byteorder='big'))
         f.write(bytes([0xff] * (4096 - 2)))
-
+        
+        #append boot.sig len
+        with open("boot.sig", "rb") as sig_file:
+            sig_len = len(sig_file.read())
+            f.write(sig_len.to_bytes(4, byteorder='big'))
+            checksum = 0
+            for i in range(sig_len):
+                checksum += sig_file.read(i)
+            f.write(checksum.to_bytes(4, byteorder='big'))
+            # append boot.sig
+            f.write(sig_file.read())
+            
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python padding_bin_file.py [file_name]")

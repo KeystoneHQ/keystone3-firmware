7edcca5e698964b6a2ed8ef17c0452e454dbf424
ww3512687 <15809188520@163.com>
2025-01-23 13:31:11 +0800

    changing function names

--- src/ui/gui_widgets/gui_scan_widgets.c
+++ src/ui/gui_widgets/gui_scan_widgets.c
@@ -119,25 +119,25 @@ void GuiScanResult(bool result, void *param)
         }
         if (g_chainType == CHAIN_BUTT) {
             if (g_qrcodeViewType == WebAuthResult) {
-                GuiCLoseCurrentWorkingView();
+                GuiCloseCurrentWorkingView();
                 GuiFrameOpenView(&g_webAuthResultView);
             }
 #ifndef BTC_ONLY
             if (g_qrcodeViewType == KeyDerivationRequest) {
                 if (!GuiCheckIfTopView(&g_homeView)) {
-                    GuiCLoseCurrentWorkingView();
+                    GuiCloseCurrentWorkingView();
                 }
                 GuiFrameOpenViewWithParam(&g_keyDerivationRequestView, NULL, 0);
             }
 #else
             if (g_qrcodeViewType == MultisigWalletImport) {
-                GuiCLoseCurrentWorkingView();
+                GuiCloseCurrentWorkingView();
                 GuiFrameOpenView(&g_importMultisigWalletInfoView);
             }
 
             if (g_qrcodeViewType == MultisigCryptoImportXpub ||
                     g_qrcodeViewType ==  MultisigBytesImportXpub) {
-                GuiCLoseCurrentWorkingView();
+                GuiCloseCurrentWorkingView();
             }
 #endif
             return;
@@ -158,7 +158,7 @@ void GuiTransactionCheckPass(void)
 {
     GuiModelTransactionCheckResultClear();
     SetPageLockScreen(true);
-    GuiCLoseCurrentWorkingView();
+    GuiCloseCurrentWorkingView();
 #ifndef BTC_ONLY
     if (g_chainType == CHAIN_ARWEAVE) {
         if (GetIsTempAccount()) {
--- src/ui/gui_widgets/setting/gui_wallet_setting_widgets.c
+++ src/ui/gui_widgets/setting/gui_wallet_setting_widgets.c
@@ -299,7 +299,7 @@ void GuiDelWallet(bool result)
 {
     GuiDeleteAnimHintBox();
     // g_waitAnimWidget.cont = NULL;
-    GuiCLoseCurrentWorkingView();
+    GuiCloseCurrentWorkingView();
     static uint16_t signal = SIG_LOCK_VIEW_VERIFY_PIN;
     LogoutCurrentAccount();
     GuiLockScreenSetFirstUnlock();
@@ -341,7 +341,7 @@ void GuiDelWalletSetup(void)
     // GuiStopCircleAroundAnimation();
     GuiDeleteAnimHintBox();
     // g_waitAnimWidget.cont = NULL; //todo
-    GuiCLoseCurrentWorkingView();
+    GuiCloseCurrentWorkingView();
     // close to g_setupView
     GuiCloseToTargetView(&g_setupView);
     // close g_setupView

23793210037d46ac8d65386788844fd5722d915f
ww3512687 <15809188520@163.com>
2025-02-24 14:29:26 +0800

    fix version

--- src/config/version.c
+++ src/config/version.c
@@ -9,11 +9,8 @@
 #define STRINGIFY(x)                        #x
 #define EXPAND(x)                           STRINGIFY(x)
 
-#ifndef BTC_ONLY
 #define SOFTWARE_VERSION_STR "Firmware v" EXPAND(SOFTWARE_VERSION_MAJOR) "." EXPAND(SOFTWARE_VERSION_MINOR) "." EXPAND(SOFTWARE_VERSION_BUILD)
-#else
-#define SOFTWARE_VERSION_STR "Firmware v" EXPAND(SOFTWARE_VERSION_BTC_ONLY_MAJOR) "." EXPAND(SOFTWARE_VERSION_MINOR) "." EXPAND(SOFTWARE_VERSION_BUILD)
-#endif
+
 #ifdef COMPILE_SIMULATOR
 const char g_softwareVersionString[] = SOFTWARE_VERSION_STR;
 #else
@@ -32,16 +29,10 @@ void GetSoftWareVersion(char *version)
 
 void GetUpdateVersionNumber(char *version)
 {
-#ifdef BTC_ONLY
-    snprintf(version, SOFTWARE_VERSION_MAX_LEN, "%d.%d.%d-B", SOFTWARE_VERSION_MAJOR, SOFTWARE_VERSION_MINOR, SOFTWARE_VERSION_BUILD);
-#endif
-
-#ifdef CYPHERPUNK_VERSION
-    snprintf(version, SOFTWARE_VERSION_MAX_LEN, "%d.%d.%d-C", SOFTWARE_VERSION_MAJOR, SOFTWARE_VERSION_MINOR, SOFTWARE_VERSION_BUILD);
-#endif
-
-#ifdef WEB3_VERSION
+#ifndef BTC_ONLY
     snprintf(version, SOFTWARE_VERSION_MAX_LEN, "%d.%d.%d", SOFTWARE_VERSION_MAJOR, SOFTWARE_VERSION_MINOR, SOFTWARE_VERSION_BUILD);
+#else
+    snprintf(version, SOFTWARE_VERSION_MAX_LEN, "%d.%d.%d-B", SOFTWARE_VERSION_MAJOR, SOFTWARE_VERSION_MINOR, SOFTWARE_VERSION_BUILD);
 #endif
 }
 

2a16ec6738eafac08c82bf75c68bc6c273544e78
ww3512687 <15809188520@163.com>
2025-03-17 16:59:35 +0800

    Merge pull request #1653 from KeystoneHQ/fix/fix_erc20
    fix: fix erc20 parse

--- tools/ota_file_maker/make_ota_file.bat
+++ tools/ota_file_maker/make_ota_file.bat
@@ -1,2 +1,2 @@
-OTA_File_Maker_Console mcu ../../build/mh1903_padding_boot.bin %1%
-ota-maker --source ../../build/mh1903.bin --destination %1%
+@REM OTA_File_Maker_Console mcu ../../build/mh1903_padding_boot.bin %1%
+ota-maker --source ../../build/mh1903_padding_boot.bin --destination %1%

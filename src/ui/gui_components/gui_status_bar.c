aa17644165505dd42376aab2a97fc27138b311c3
ww3512687 <15809188520@163.com>
2025-03-25 10:56:40 +0800

    fix: uncomment AVAX coin integration in GUI status bar

--- src/ui/gui_components/gui_status_bar.c
+++ src/ui/gui_components/gui_status_bar.c
@@ -71,7 +71,7 @@ const static CoinWalletInfo_t g_coinWalletBtn[] = {
     {CHAIN_ETH, "", &coinEth},       {CHAIN_SOL, "", &coinSol},
     {CHAIN_BNB, "", &coinBnb},       {CHAIN_HNT, "", &coinHelium},
     {CHAIN_XRP, "", &coinXrp},
-    //  {CHAIN_AVAX, "", &coinAva},
+    {CHAIN_AVAX, "", &coinAva},
     {CHAIN_ADA, "", &coinAda},       {CHAIN_TON, "", &coinTon},
     {CHAIN_TRX, "", &coinTrx},       {CHAIN_LTC, "", &coinLtc},
     {CHAIN_BCH, "", &coinBch},       {CHAIN_APT, "", &coinApt},

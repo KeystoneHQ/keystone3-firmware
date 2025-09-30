d1a016af051dab97db41de30ccb32aed05687d9d
ww3512687 <15809188520@163.com>
2025-09-30 11:33:29 +0800

    fix ui bugs

--- src/ui/gui_widgets/multi/web3/gui_general_home_widgets.c
+++ src/ui/gui_widgets/multi/web3/gui_general_home_widgets.c
@@ -492,7 +492,7 @@ static void GuiUpdateCoinListWidget(HomeGesture_t gesture)
     }
 
     if (GetMnemonicType() == MNEMONIC_TYPE_TON) {
-        lv_obj_t *label = GuiCreateIllustrateLabel(coinListCont, _("import_ton_mnemonic_desc"));
+        lv_obj_t *label = GuiCreateIllustrateLabel(coinListCont, _("ton_assets_manage_notice"));
         lv_obj_set_width(label, 416);
         lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
         lv_obj_align(label, LV_ALIGN_TOP_LEFT, 32, 144);

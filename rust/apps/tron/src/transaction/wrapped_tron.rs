311f41a692eb6b5dffe2bddb0073717d8889f71b
qkin <qkin-git@keyst.one>
2026-03-16 11:06:24 +0800

    fix:add memo for tron swap tx

--- rust/apps/tron/src/transaction/wrapped_tron.rs
+++ rust/apps/tron/src/transaction/wrapped_tron.rs
@@ -37,6 +37,7 @@ pub struct WrappedTron {
     pub(crate) value: String,
     pub(crate) token_short_name: Option<String>,
     pub(crate) divider: f64,
+    pub(crate) memo: String,
 }
 
 #[macro_export]
@@ -286,11 +287,18 @@ impl WrappedTron {
                     token_short_name = Some(value.token_short_name);
                     divider = 10u64.pow(value.decimals as u32) as f64;
                 }
-                let tron_tx = if tx.contract_address.is_empty() {
+                let mut tron_tx = if tx.contract_address.is_empty() {
                     Self::build_transfer_tx(tx)
                 } else {
                     Self::generate_trc20_tx(tx)
                 }?;
+
+                if !tx.memo.is_empty() {
+                    if let Some(ref mut raw) = tron_tx.raw_data {
+                        raw.data = tx.memo.as_bytes().to_vec();
+                    }
+                }
+
                 Ok(Self {
                     hd_path: content.hd_path,
                     extended_pubkey: context.extended_public_key.to_string(),
@@ -303,6 +311,7 @@ impl WrappedTron {
                     value: tx.value.to_string(),
                     divider,
                     token_short_name,
+                    memo: tx.memo.clone(),
                 })
             }
             _ => Err(TronError::InvalidRawTxCryptoBytes(
@@ -444,5 +453,15 @@ mod tests {
             result.unwrap_err(),
             TronError::InvalidParseContext(_)
         ));
+    }
+
+    #[test]
+    fn test_payload_with_memo() {
+        let hex = "1f8b080000000000000365ce3f4fc2400005f0508d121695c93091c60463527bff7ad76b6222b4481310150e81c9f47a2501954b8988f11b39b93b38b96af8087e020775711357fce54def2d2f6be437ce26be5649f174a26f74acaf0aaf46d6c8671de824d82591f96ce45645eba499dfe112aa44126561c9944564a42c2e95b290048a49ac62495561ebda26a46443ee946cb0880d8a8f1fef4f3f6077adf295c9ad8856af608ad630ad77e96567380b6f1b32ad55d376ea87a1821d3e9d0d6aba6c9ae27caccb7d41fab3a847184cfc6697dc4f477d5e81ba4ebaf120dc6b1c785511ee77da81f0c01d238862aadca34a807ca80286602029e492f0240a9043b0eb048c290fbb9042006c6c03cfbab0c7b1871637d1faa2fbe31de70ec1321e0f8044903246204ee27ffb92cdefcfccf67cfef2f0867f012b09fb6163010000";
+        let pubkey_str = "xpub6D1AabNHCupeiLM65ZR9UStMhJ1vCpyV4XbZdyhMZBiJXALQtmn9p42VTQckoHVn8WNqS7dqnJokZHAHcHGoaQgmv8D45oNUKx6DZMNZBCd";
+        let payload = prepare_payload(hex);
+        let context = prepare_parse_context(pubkey_str);
+        let tx = WrappedTron::from_payload(payload, &context).unwrap();
+        assert_eq!("=:ETH.USDT:0x742636d8FBD2C1dD721Db619b49eaD254385D77d:3816100/3/0:-_/nc:20/0", tx.memo);
     }
 }

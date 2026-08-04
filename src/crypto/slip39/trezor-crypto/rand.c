e83b42a1b0aca86bff0ba4c8c543c1bb90a6b9f5
Soralit <soralitria@gmail.com>
2026-08-04 16:08:37 +0800

    Revert "Remove insecure PRNG fallback"

--- src/crypto/slip39/trezor-crypto/rand.c
+++ src/crypto/slip39/trezor-crypto/rand.c
@@ -23,12 +23,72 @@
 
 #include "rand.h"
 
-// Only used by the simulator.
-#ifdef COMPILE_SIMULATOR
-void random_buffer(uint8_t *buf, size_t len)
+#ifndef RAND_PLATFORM_INDEPENDENT
+
+/*
+#pragma message( \
+    "NOT SUITABLE FOR PRODUCTION USE! Replace random32() function with your own secure code.")
+*/
+
+// The following code is not supposed to be used in a production environment.
+// It's included only to make the library testable.
+// The message above tries to prevent any accidental use outside of the test
+// environment.
+//
+// You are supposed to replace the random8() and random32() function with your
+// own secure code. There is also a possibility to replace the random_buffer()
+// function as it is defined as a weak symbol.
+
+static uint32_t seed = 0;
+
+void random_reseed(const uint32_t value)
 {
+    seed = value;
+}
+
+uint32_t random32(void)
+{
+    // Linear congruential generator from Numerical Recipes
+    // https://en.wikipedia.org/wiki/Linear_congruential_generator
+    seed = 1664525 * seed + 1013904223;
+    return seed;
+}
+
+#endif /* RAND_PLATFORM_INDEPENDENT */
+
+//
+// The following code is platform independent
+//
+
+#ifndef COMPILE_SIMULATOR
+void __attribute__((weak)) random_buffer(uint8_t *buf, size_t len)
+#else
+void random_buffer(uint8_t *buf, size_t len)
+#endif
+{
+    uint32_t r = 0;
     for (size_t i = 0; i < len; i++) {
-        buf[i] = rand() & 0xFF;
+        if (i % 4 == 0) {
+            r = random32();
+        }
+        buf[i] = (r >> ((i % 4) * 8)) & 0xFF;
     }
 }
-#endif
+
+uint32_t random_uniform(uint32_t n)
+{
+    uint32_t x, max = 0xFFFFFFFF - (0xFFFFFFFF % n);
+    while ((x = random32()) >= max)
+        ;
+    return x / (max / n);
+}
+
+void random_permute(char *str, size_t len)
+{
+    for (int i = len - 1; i >= 1; i--) {
+        int j = random_uniform(i + 1);
+        char t = str[j];
+        str[j] = str[i];
+        str[i] = t;
+    }
+}
